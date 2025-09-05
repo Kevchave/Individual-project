1255a831a4e04c209bbeaad26f28e035d219eefa
Kevin <kevin12chave@gmail.com>
2025-09-05 12:25:22 +0100

    Changed out-of-bounds

--- static/js/polling.js
+++ static/js/polling.js
@@ -23,21 +23,21 @@ function updateOutOfBounds(wpm, volume, pitch, dataPointsCount) {
     }
 
     // WPM Boundaries 
-    if (wpm > 240 || wpm < 80) {
+    if (wpm > 200 || wpm < 100) {
         wpmBox.classList.add('out-of-bounds');
     } else {
         wpmBox.classList.remove('out-of-bounds');
     }
 
     // Volume Boundaries  
-    if (volume < -40) {
+    if (volume < -50) {
         volumeBox.classList.add('out-of-bounds');
     } else {
         volumeBox.classList.remove('out-of-bounds');
     }
 
     // Pitch Boundaries
-    if (pitch < 5) {
+    if (pitch < 10) {
         pitchBox.classList.add('out-of-bounds');
     } else {
         pitchBox.classList.remove('out-of-bounds');
